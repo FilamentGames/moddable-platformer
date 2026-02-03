e2e3f65acf79adb475edcc5c9425700b9f17f1cc
Alex Stone <stone@filamentgames.com>
2026-02-02 21:15:30 -0600

    convert to using add_dock api

--- addons/bgd/plugin.gd
+++ addons/bgd/plugin.gd
@@ -1,26 +1,24 @@
 @tool
 extends EditorPlugin
 
-var quest_dock: Control
+var quest_dock: EditorDock
 
 func _enter_tree():
 	print("Baby Godot plugin enabled")
 	
-	# Load and instantiate the quest dock scene
 	var quest_dock_scene = load("res://addons/bgd/quest_dock.tscn")
-	if quest_dock_scene:
-		quest_dock = quest_dock_scene.instantiate()
-		# Add the dock to the bottom panel (with the debugger, output, etc.)
-		add_control_to_bottom_panel(quest_dock, "Quest")
+	quest_dock = EditorDock.new()
+	quest_dock.title = "Quest"
+	quest_dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
+	var quest_dock_content = quest_dock_scene.instantiate()
+	quest_dock.add_child(quest_dock_content)
+	add_dock(quest_dock)
 
-	else:
-		push_error("Failed to load quest_dock.tscn")
 
 func _exit_tree():
 	print("Baby Godot plugin disabled")
 	
 	# Remove the dock when plugin is disabled
 	if quest_dock:
-		remove_control_from_bottom_panel(quest_dock)
+		remove_dock(quest_dock)
 		quest_dock.queue_free()
-		quest_dock = null
