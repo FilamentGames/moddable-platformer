59695bc25ac2f2a216f72a7aae043540ba5ce37f
Natalie Martin <nmartin@filamentgames.com>
2026-06-08 14:15:55 -0500

    Implement proof-of-concept editor and game communication in the level
    Create some test objects that communicate with the editor in some way
    and place them in the level.
    
    The `test_display_quest_text` object shows off editor-to-game
    communication. There are two of them to show off that multiplexing
    works.
    
    The `test_progress_quest` object shows off game-to-editor communication.

---
+++ components/babygodot/test_display_quest_text.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var label: Label
+
+var bridge: InGameQuestsBridge = InGameQuestsBridge.new()
+
+func _ready():
+	bridge.quest_text.connect(_update_quest_text)
+	bridge.request_quest_text()
+
+func _update_quest_text(text: String):
+	label.text = text
