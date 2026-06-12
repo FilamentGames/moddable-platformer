5ce19a064e16229f363b9ed29681b7f502335f5d
Natalie Martin <nmartin@filamentgames.com>
2026-06-12 13:50:42 -0500

    Set up basic checkpoint saving/loading system
    Make it so we can save a checkpoint and load it from the plugin UI. This
    current checkpoint system uses the filesystem to make saving/loading a
    scene a bit easier for testing around with the plugin, though we can
    save it in memory as well if needed.

--- addons/quests/main.gd
+++ addons/quests/main.gd
@@ -10,6 +10,8 @@ var dock_scene: BabyGodotQuestDock
 ## Holds the instance of the quests bridge debugger plugin
 var bridge: BabyGodotQuestsBridge
 
+var checkpoints: CheckpointHelper = CheckpointHelper.new()
+
 const global_message_service_name = "GlobalMessagingService"
 
 func _enable_plugin() -> void:
@@ -18,8 +20,17 @@ func _enable_plugin() -> void:
 func _disable_plugin() -> void:
 	remove_autoload_singleton(global_message_service_name)
 
+func save_editor_scene_as_checkpoint() -> void:
+	checkpoints.save_editor_scene_as_checkpoint()
+
+func set_editor_scene() -> void:
+	checkpoints.set_editor_scene()
+
 func _enter_tree() -> void:
+	checkpoints.plugin = self
+
 	var quests: BabyGodotQuests = preload("res://addons/quests/quests/quests.tscn").instantiate()
+	quests.editor_scene_provider = self
 	GlobalQuests.quests = quests
 	dock_scene = preload("res://addons/quests/quest_dock/quest_dock.tscn").instantiate()
 
--- addons/quests/quest_dock/quest_dock.gd
+++ addons/quests/quest_dock/quest_dock.gd
@@ -8,6 +8,10 @@ class_name BabyGodotQuestDock
 ## The Button control that moves to the next step. This might end up being a debug-only control?
 @export var next_button: Button
 
+@export var save_checkpoint_button: Button
+
+@export var load_checkpoint_button: Button
+
 ## The instance of BabyGodotQuests object we're getting data from.
 var quests_provider: BabyGodotQuests
 
@@ -19,6 +23,10 @@ func _enter_tree() -> void:
 	update_text()
 	quests_provider.text_updated.connect(update_text)
 	next_button.pressed.connect(next)
+	if save_checkpoint_button:
+		save_checkpoint_button.pressed.connect(save_checkpoint)
+	if load_checkpoint_button:
+		load_checkpoint_button.pressed.connect(load_checkpoint)
 
 func update_text():
 	text.text = quests_provider.get_current_text()
@@ -26,3 +34,9 @@ func update_text():
 func next():
 	quests_provider.next()
 	next_button.disabled = !quests_provider.can_proceed()
+
+func save_checkpoint():
+	quests_provider.save_checkpoint()
+
+func load_checkpoint():
+	quests_provider.load_checkpoint()
