ea2338695e6aa38b2d90dd38b84a9b2428c7f7d6
Natalie Martin <nmartin@filamentgames.com>
2026-06-23 17:15:22 -0500

    Delete commented out code
    oops I accidentally checked this in

--- components/babygodot/dialogue_box/dialogue_box.gd
+++ components/babygodot/dialogue_box/dialogue_box.gd
@@ -18,10 +18,6 @@ signal finished
 func _ready() -> void:
 	_get_next_text()
 
-# func _process(_delta: float) -> void:
-# 	if Input.is_action_just_pressed("player_action"):
-# 		_get_next_text()
-
 ## Loads the next text or queues for deletion when there is no more text to fetch
 func _get_next_text() -> void:
 	if dialogue_lines.size() == 0:
