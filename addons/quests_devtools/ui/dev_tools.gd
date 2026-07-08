9df13b1e8c7c060ada1c1d1b25c08e649ca56acf
Natalie Martin <nmartin@filamentgames.com>
2026-07-08 13:16:02 -0500

    Add dock show/hide commands to dev tools plugin
    Instead of adding confusing triggers in-game to show/hide the docks as a
    proof of concept, just use buttons in our fancy dev tools plugin to
    activate these instead.
    
    We don't know where these functions will be dispatched in the "final"
    version of the game so it probably makes the most sense just to keep
    these calls in dev tools for now until they need to be dispatched in
    game.

--- addons/quests_devtools/ui/dev_tools.gd
+++ addons/quests_devtools/ui/dev_tools.gd
@@ -8,3 +8,20 @@ func _ready() -> void:
 func _click_button() -> void:
 	if GlobalQuests and GlobalQuests.quests:
 		GlobalQuests.quests.reset_progress()
+		
+func _get_streamlined_exclusive_dock(name: StringName):
+	var interface = EditorInterface
+	if interface.has_method(name):
+		return interface.call(name)
+	else:
+		return null
+
+
+func _toggle_streamlined_exclusive_dock(name: StringName, visible: bool) -> void:
+	var dock: EditorDock = _get_streamlined_exclusive_dock(name)
+	if dock:
+		dock.open() if visible else dock.close()
+	else:
+		# Print a message to the output window so we know it would have worked in streamlined Godot
+		var verb := "open" if visible else "close"
+		print("Tried to " + verb + " the dock via `" + name + "` but it's not possible in this version of Godot!")
