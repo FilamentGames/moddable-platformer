8fac2e4e70b61eabe877f7747af1448abc96da1d
Nate Marshall <nmarshall@filamentgames.com>
2026-07-31 14:01:04 -0400

    fix typing issue causing things to break

--- addons/quests/main.gd
+++ addons/quests/main.gd
@@ -119,8 +119,8 @@ func _auto_play_when_ready() -> void:
 	var main_scene: String = ProjectSettings.get_setting("application/run/main_scene")
 	var deadline_msec = Time.get_ticks_msec() + int(auto_play_ready_timeout_sec * 1000.0)
 
-	var web_timeout: double = 10.0
-	var desktop_timeout: double = 0.5
+	var web_timeout = 10.0
+	var desktop_timeout = 0.5
 
 	while Time.get_ticks_msec() < deadline_msec:
 		if not filesystem.is_scanning() and not filesystem.is_importing():
