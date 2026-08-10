89c4ce3ef6fbf53a216a4edb0a94b782d767453e
Natalie Martin <nmartin2473@gmail.com>
2026-08-10 09:30:42 -0500

    Make first yarnspark have the collectible property on it
    Instead of having the player choose the nebulous condition object, have
    them able to click the yarnspark itself and have a checkbox on it. Also
    we can now use the object property equality checker instead of the
    initial inspector checkbox thing.

---
+++ components/babygodot/yarnspark/subtypes/intro_yarnspark.gd
@@ -0,0 +1,7 @@
+@tool
+extends Collectible
+class_name IntroYarnspark
+## A special instance of the Yarnspark that has a "Can I get the Yarnspark?" property on it, to introduce the concept of the inspector to new players.
+
+## Check this box to make it so you can pick up the Yarnspark!
+@export var can_i_get_the_yarnspark: bool = false
