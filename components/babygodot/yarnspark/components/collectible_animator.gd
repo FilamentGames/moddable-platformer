422a70b0fdc17ab2dda7419158b2c63e651c01f1
Natalie Martin <nmartin@filamentgames.com>
2026-08-11 14:36:48 -0500

    Add yarnspark enable/disable sprite animations
    Instead of using an animator, just use a script that enables/disables
    the correct nodes timed with the sprite animation.

--- components/babygodot/yarnspark/components/collectible_animator.gd
+++ components/babygodot/yarnspark/components/collectible_animator.gd
@@ -2,13 +2,23 @@
 extends Node
 class_name ScrollAnimator
 
-## The sprite to show
-@export var animator: AnimationPlayer
+## The path that contains the collectible yarnspark sprite
+@export var yarnspark_node: Node
+
+## The path that contains the uncollectible string sprite
+@export var string_node: Node
+
+## The animated sprite that plays animations
+@export var animated_sprite: AnimatedSprite2D
 
 func _trigger_animation(condition_met: bool) -> void:
-	animator.play(_get_animation_name(condition_met))
+	yarnspark_node.hide()
+	string_node.show()
+	animated_sprite.play(_get_sprite_animation_name(condition_met))
+	await animated_sprite.animation_finished
+	yarnspark_node.visible = condition_met
+	string_node.visible = not condition_met
+	animated_sprite.play(&"default")
 
-func _get_animation_name(condition_met: bool) -> StringName:
-	if condition_met:
-		return &"activate_scroll"
-	return &"deactivate_scroll"
+func _get_sprite_animation_name(condition_met: bool) -> StringName:
+	return &"enable" if condition_met else &"disable"
