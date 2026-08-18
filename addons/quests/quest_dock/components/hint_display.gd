46277ad7a5c6c4847940bff9135a3880f1bf26c8
Natalie Martin <nmartin@filamentgames.com>
2026-08-18 10:17:23 -0500

    Use pause/play symbols in hint text
    For some reason these don't load on web builds so just use the icons.
    This means that the hint text is now rich text and supports BBCode.

--- addons/quests/quest_dock/components/hint_display.gd
+++ addons/quests/quest_dock/components/hint_display.gd
@@ -7,7 +7,7 @@ class_name HintDisplay
 
 @export_group("Internal Refs")
 ## The label that displays the hint text
-@export var label: Label
+@export var label: RichTextLabel
 
 func _ready():
 	_update_label_text()
